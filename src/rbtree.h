d32b46db8d9f86fbf8cd95de4bf243e99d1e8707
jeonje <whssodi@gmail.com>
2022-11-28 20:29:39 +0900

    test_erase_root 무한반복

--- src/rbtree.h
+++ src/rbtree.h
@@ -22,13 +22,25 @@ rbtree *new_rbtree(void);
 void delete_rbtree(rbtree *);
 
 node_t *rbtree_insert(rbtree *, const key_t);
-node_t *rbtree_insert_fixup(rbtree *, node_t* z);
+
 node_t *rbtree_find(const rbtree *, const key_t);
 node_t *rbtree_min(const rbtree *);
 node_t *rbtree_max(const rbtree *);
 int rbtree_erase(rbtree *, node_t *);
 
 int rbtree_to_array(const rbtree *, key_t *, const size_t);
-int in_order(node_t *, key_t *, const size_t, int, const rbtree *);
+
+
+//add function
+node_t *rbtree_insert_fixup(rbtree *, node_t*);
+
+void rbtree_erase_fixup(rbtree *, node_t *);
+void delete_one(rbtree*, node_t *);
+void rb_transplant(rbtree *, node_t *, node_t *);
+node_t *tree_minimum(rbtree *, node_t *);
+
+int in_order(const rbtree *, node_t *, key_t *, const size_t, int );
+
+
 
 #endif  // _RBTREE_H_
